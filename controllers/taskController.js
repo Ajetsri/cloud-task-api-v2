1d29ec72302184b84e3eacd6e7691c24a0443a6f
Ajetsri <sri291399@gmail.com>
2026-07-26 22:35:23 +0530

    all crud operation done

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -39,9 +39,92 @@ const getAllTasks = async(req, res) => {
 
     }
 };
+// Get Task By ID
+const getTaskByID = async(req,res) => {
+    try{
+        const task = await Task.findById(req.params.id);
+
+        if(!task){
+            return res.status(404).json({
+                success : false,
+                data :"Task not Found"
+            });
+        }
+        res.status(200).json({
+            success : true,
+            data :task
+        });
+    }
+    catch(error){
+        res.status(500).json({
+            success :false,
+            message : error.message
+        });
+
+    }
+};
+// Update Task
+const updateTask =async(req,res) => {
+    try{
+        const task = await Task.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {
+                new : true,
+                runValidators : true
+            }
+        );
+        if(!Task){
+            return res.status(404).json({
+                success : false,
+                message :"Task not found"
+            });
+        }
+        res.status(200).json({
+            success : true,
+            data : task
+        });
+    }
+    catch(error){
+        res.status(500).json({
+            success : false,
+            message : error.message
+        });
+
+    }
+};
+//delete Task
+const deleteTask = async(req,res) => {
+    try{
+        const task =await Task.findByIdAndDelete(req.params.id);
+        if(!task){
+            return res.status(404).json({
+                success : false,
+                message: "Task not Found"
+            });
+        }
+        res.status(200).json({
+            success : true,
+            message :"Task deleted successfully"
+            
+        });
+
+    }
+    catch(error){
+        res.status(500).json({
+            success : false,
+            message : error.message
+        });
+
+    }
+
+};
 module.exports = {
     createTask,
-    getAllTasks 
+    getAllTasks,
+    getTaskByID,
+    updateTask,
+    deleteTask
 };
 
 
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -2,12 +2,18 @@ const express = require("express");
 
 const{
     createTask,
-    getAllTasks 
+    getAllTasks,
+    getTaskByID,
+    updateTask,
+    deleteTask
 }= require("../controllers/taskController");
 
 const router=express.Router();
 router.post("/",createTask);
 router.get("/",getAllTasks);
+router.get("/:id",getTaskByID);
+router.put("/:id",updateTask);
+router.delete("/:id",deleteTask);
 
 module.exports = router;
 
