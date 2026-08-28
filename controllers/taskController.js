04db6433167ecda55c187713fd25c5cb9adf34b1
Ajetsri <sri291399@gmail.com>
2026-08-28 22:05:40 +0530

    filter

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -12,28 +12,53 @@ const createTask= asyncHandler(async(req, res) => {
         });
 });
 //getAllTasks 
-const getAllTasks = asyncHandler(async(req, res) => {
-        const page = parseInt(req.query.page)||1;
-        const limit = parseInt(req.query.limit)||10;
-        const skip = (page - 1) * limit;
-        const totalTasks = await Task.countDocuments();//Get total number of tasks
-        const totalPages = Math.ceil(totalTasks / limit);//Calculate total pages
+const getAllTasks = asyncHandler(async (req, res) => {
 
-        const tasks = await Task.find()
-                                .skip(skip)
-                                .limit(limit);
-        
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
 
-        res.status(200).json({
-            success: true,
-            count: tasks.length,
-            page: page,
-            limit: limit,
-            totalTasks: totalTasks,
-            totalPages: totalPages,
-            data: tasks
-        });
+    const skip = (page - 1) * limit;
 
+    const sort = req.query.sort || "-createdAt";
+//valid or not
+    const allowedSortFields = [
+        "createdAt",
+        "task",
+        "completed"
+    ];
+//sort
+    const sortField = sort.replace("-", "");
+
+    if (!allowedSortFields.includes(sortField)) {
+        throw new AppError("Invalid sort field", 400);
+    }
+    //filter
+    const filter = {};
+    
+    if (req.query.completed !== undefined) {
+        if (req.query.completed !== "true" && req.query.completed !== "false") {
+                throw new AppError("completed must be true or false", 400);
+            }
+            filter.completed = req.query.completed === "true";
+        }
+    const tasks = await Task.find(filter)
+        .sort(sort)
+        .skip(skip)
+        .limit(limit);
+
+    const totalTasks = await Task.countDocuments();
+
+    const totalPages = Math.ceil(totalTasks / limit);
+
+    res.status(200).json({
+        success: true,
+        page,
+        limit,
+        count: tasks.length,
+        totalTasks,
+        totalPages,
+        data: tasks
+    });
 });
 // Get Task By ID
 const getTaskByID = asyncHandler(async(req,res) => {
